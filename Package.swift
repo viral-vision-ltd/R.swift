77a3e39c3a02b4cc1d1adc033606dd1871d0158d
Tien Nguyen <lodkat01@gmail.com>
2023-07-27 14:42:46 +0300

    New generator

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .library(name: "RswiftLibrary", targets: ["RswiftResources"]),
         .plugin(name: "RswiftGenerateInternalResources", targets: ["RswiftGenerateInternalResources"]),
         .plugin(name: "RswiftGeneratePublicResources", targets: ["RswiftGeneratePublicResources"]),
+        .plugin(name: "RswiftGeneratePublicImageStringResources", targets: ["RswiftGeneratePublicImageStringResources"]),
         .plugin(name: "RswiftGenerateResourcesCommand", targets: ["RswiftGenerateResourcesCommand"]),
         .plugin(name: "RswiftModifyXcodePackages", targets: ["RswiftModifyXcodePackages"]),
     ],
